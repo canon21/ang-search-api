3ef6dc78863f4a9b2e41dff803bf519de28a321a
Tommaso Pirozzi <tommasopirozzi@hotmail.it>
2021-05-29 13:19:17 +0200

    Adding pass data from child to parent and some markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wsearch-api';
+
+  //questo è uno shortcut. Vedi la forma completa annotata con ++
+  constructor(private wikipedia: WikipediaService) {}
+
+  // ++
+  /*
+    private wikipedia: WikipediaService;
+
+    constructor(wikipedia: WikipediaService) {
+        this.wikipedia = wikipedia;
+    }
+  */
+
+  onTerm(term: string){
+    const result = this.wikipedia.search(term);
+    console.log(result);
+  }
 }
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  /* Senza type inference
+  term : string = '' ;
+  */
+
+  @Output() submitted = new EventEmitter<string>();
+
+  //Con type inference.
+  term = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  //Vedi component.html -> c'è un modo alternativo di fare l'assegnazione direttamente nel componente.html
+  /*
+  onInput(value: string) {
+    this.term = value;
+  }
+  */
+
+  
+  //child to parent comunication
+  onFormSubmit(event: any) {
+    event.preventDefault();
+    //child to parent comunication
+    this.submitted.emit(this.term);
+  }
 }
